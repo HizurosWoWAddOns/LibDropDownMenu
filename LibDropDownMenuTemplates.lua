d3dad1d1fb39936aab97527c349452f3e5efa44b
Hizuro <hizuro@gmx.net>
2019-03-18 07:41:28 +0100

    update to blizzards UIDropDownMenu from Build 29737

--- LibDropDownMenuTemplates.lua
+++ LibDropDownMenuTemplates.lua
@@ -1,41 +1,6 @@
 
 -- Replacement of LibDropDownMenu.xml
-
 local lib = LibStub("LibDropDownMenu");
-
-local function buttonOnClick(self)
-	lib.UIDropDownMenuButton_OnClick(self, button, down);
-end
-
-local function buttonOnEnter(self)
-	if ( self.hasArrow ) then
-		local level =  self:GetParent():GetID() + 1;
-		local listFrame = _G["LibDropDownMenu_List"..level];
-		if ( not listFrame or not listFrame:IsShown() or select(2, listFrame:GetPoint()) ~= self ) then
-			lib.ToggleDropDownMenu(level, self.value, nil, nil, nil, nil, self.menuList, self.arrow);
-		end
-	else
-		lib.CloseDropDownMenus(self:GetParent():GetID() + 1);
-	end
-	lib.UIDropDownMenu_StopCounting(self:GetParent());
-	_G[self:GetName().."Highlight"]:Show();
-	if ( self.tooltipTitle ) then
-		if ( self.tooltipOnButton ) then
-			GameTooltip:SetOwner(self, "ANCHOR_RIGHT");
-			GameTooltip:AddLine(self.tooltipTitle, 1.0, 1.0, 1.0);
-			GameTooltip:AddLine(self.tooltipText, nil, nil, nil, true);
-			GameTooltip:Show();
-		else
-			GameTooltip_AddNewbieTip(self, self.tooltipTitle, 1.0, 1.0, 1.0, self.tooltipText, 1);
-		end
-	end
-end
-
-local function buttonOnLeave(self)
-	_G[self:GetName().."Highlight"]:Hide();
-	lib.UIDropDownMenu_StartCounting(self:GetParent());
-	GameTooltip:Hide();
-end
 
 local function buttonOnEnable(self)
 	self.invisibleButton:Hide();
@@ -46,14 +11,14 @@ local function buttonOnDisable(self)
 end
 
 local function buttonColorSwatchOnClick(self)
-	lib.CloseDropDownMenus(self:GetParent():GetParent():GetID() + 1);
-	_G[self:GetName().."SwatchBg"]:SetVertexColor(NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b);
-	lib.UIDropDownMenu_StopCounting(self:GetParent():GetParent());
+	CloseMenus();
+	lib.UIDropDownMenuButton_OpenColorPicker(self:GetParent());
 end
 
 local function buttonColorSwatchOnEnter(self)
-	CloseMenus();
-	lib.UIDropDownMenuButton_OpenColorPicker(self:GetParent());
+	lib.CloseDropDownMenus(self:GetParent():GetParent():GetID() + 1);
+	_G[self:GetName().."SwatchBg"]:SetVertexColor(NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b);
+	lib.UIDropDownMenu_StopCounting(self:GetParent():GetParent());
 end
 
 local function buttonColorSwatchOnLeave(self)
@@ -61,41 +26,20 @@ local function buttonColorSwatchOnLeave(self)
 	lib.UIDropDownMenu_StartCounting(self:GetParent():GetParent());
 end
 
-local function buttonInvisibleButtonOnEnter(self)
-	lib.UIDropDownMenu_StopCounting(self:GetParent():GetParent());
-	lib.CloseDropDownMenus(self:GetParent():GetParent():GetID() + 1);
-	local parent = self:GetParent();
-	if ( parent.tooltipTitle and parent.tooltipWhileDisabled) then
-		if ( parent.tooltipOnButton ) then
-			GameTooltip:SetOwner(parent, "ANCHOR_RIGHT");
-			GameTooltip:AddLine(parent.tooltipTitle, 1.0, 1.0, 1.0);
-			GameTooltip:AddLine(parent.tooltipText, nil, nil, nil, true);
-			GameTooltip:Show();
-		else
-			GameTooltip_AddNewbieTip(parent, parent.tooltipTitle, 1.0, 1.0, 1.0, parent.tooltipText, 1);
-		end
-	end
+local function buttonExpandArrowOnClick(self, button, down)
+	lib.ToggleDropDownMenu(self:GetParent():GetParent():GetID() + 1, self:GetParent().value, nil, nil, nil, nil, self:GetParent().menuList, self);
 end
 
-local function buttonInvisibleButtonOnLeave(self)
-	lib.UIDropDownMenu_StartCounting(self:GetParent():GetParent());
-	GameTooltip:Hide();
+local function buttonExpandArrowOnEnter(self)
+	lib.UIDropDownMenuButton_OnEnter(self:GetParent());
+end
+
+local function buttonExpandArrowOnLeave(self)
+	lib.UIDropDownMenuButton_OnLeave(self:GetParent());
 end
 
 local function listOnClick(self)
 	self:Hide();
-end
-
-local function listOnEnter(self,motion)
-	lib.UIDropDownMenu_StopCounting(self, motion);
-end
-
-local function listOnLeave(self,motion)
-	lib.UIDropDownMenu_StartCounting(self, motion);
-end
-
-local function listOnUpdate(self,elapsed)
-	lib.UIDropDownMenu_OnUpdate(self, elapsed);
 end
 
 local function listOnShow(self)
@@ -111,10 +55,6 @@ local function listOnShow(self)
 	if ( self:GetID() > 1 ) then
 		self.parent = _G["LibDropDownMenu_List"..(self:GetID() - 1)];
 	end
-end
-
-local function listOnHide(self)
-	lib.UIDropDownMenu_OnHide(self);
 end
 
 local function menuOnHide(self)
@@ -142,15 +82,59 @@ local function menuButtonOnClick(self,button)
 	PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON);
 end
 
-local templates = {};
+local UIDropDownCustomMenuEntryMixin = {};
+
+function UIDropDownCustomMenuEntryMixin:GetPreferredEntryWidth()
+	-- NOTE: Only width is currently supported, dropdown menus size vertically based on how many buttons are present.
+	return self:GetWidth();
+end
+
+function UIDropDownCustomMenuEntryMixin:OnSetOwningButton()
+	-- for derived objects to implement
+end
+
+function UIDropDownCustomMenuEntryMixin:SetOwningButton(button)
+	self:SetParent(button:GetParent());
+	self.owningButton = button;
+	self:OnSetOwningButton();
+end
+
+function UIDropDownCustomMenuEntryMixin:GetOwningDropdown()
+	return self.owningButton:GetParent();
+end
+
+function UIDropDownCustomMenuEntryMixin:SetContextData(contextData)
+	self.contextData = contextData;
+end
+
+function UIDropDownCustomMenuEntryMixin:GetContextData()
+	return self.contextData;
+end
+
+local function customMenuEntryOnEnter(self)
+	lib.UIDropDownMenu_StopCounting(self:GetOwningDropdown());
+end
+
+local function customMenuEntryOnLeave(self)
+	lib.UIDropDownMenu_StartCounting(self:GetOwningDropdown());
+end
+
+function lib.Create_DropDownCustomMenuEntry(name,parent,opts)
+	local frame = CreateFrame("frame");
+	frame:EnableMouse(true);
+	frame:Hide();
+	frame:SetScript("OnEnter",customMenuEntryOnEnter);
+	frame:SetScript("OnLeave",customMenuEntryOnLeave);
+	Mixin(frame,UIDropDownCustomMenuEntryMixin);
+end
 
 function lib.Create_DropDownMenuButton(name,parent,opts)
 	local button = CreateFrame("Button",name,parent);
-	button:SetSize(180,16);
+	button:SetSize(100,16);
 	button:SetFrameLevel(parent:GetFrameLevel()+2); -- OnLoad
-	button:SetScript("OnClick",buttonOnClick);
-	button:SetScript("OnEnter",buttonOnEnter);
-	button:SetScript("OnLeave",buttonOnLeave);
+	button:SetScript("OnClick",lib.UIDropDownMenuButton_OnClick);
+	button:SetScript("OnEnter",lib.UIDropDownMenuButton_OnEnter);
+	button:SetScript("OnLeave",lib.UIDropDownMenuButton_OnLeave);
 	button:SetScript("OnEnable",buttonOnEnable);
 	button:SetScript("OnDisable",buttonOnDisable);
 
@@ -166,29 +150,34 @@ function lib.Create_DropDownMenuButton(name,parent,opts)
 	button:SetNormalFontObject("GameFontHighlightSmallLeft")
 	button:SetHighlightFontObject("GameFontHighlightSmallLeft");
 	button:SetDisabledFontObject("GameFontDisableSmallLeft");
+	button.NormalText = text;
 
 	local highlight = button:CreateTexture(name.."Highlight","BACKGROUND");
 	highlight:SetTexture([[Interface\QuestFrame\UI-QuestTitleHighlight]]);
 	highlight:SetBlendMode("ADD");
 	highlight:SetAllPoints();
 	highlight:Hide();
+	button.Highlight = highlight;
 
 	local check = button:CreateTexture(name.."Check","ARTWORK");
 	check:SetTexture([[Interface\Common\UI-DropDownRadioChecks]]);
 	check:SetSize(16,16);
 	check:SetPoint("LEFT",0,0);
 	check:SetTexCoord(0,0.5,0.5,1);
+	--button.Check = check;
 
 	local uncheck = button:CreateTexture(name.."UnCheck","ARTWORK");
 	uncheck:SetTexture([[Interface\Common\UI-DropDownRadioChecks]]);
 	uncheck:SetSize(16,16);
 	uncheck:SetPoint("LEFT",0,0);
 	uncheck:SetTexCoord(0.5,1,0.5,1);
+	--button.UnCheck = uncheck;
 
 	local icon = button:CreateTexture(name.."Icon","ARTWORK");
 	icon:Hide();
 	icon:SetSize(16,16);
 	icon:SetPoint("RIGHT",0,0);
+	button.Icon = icon;
 
 	local color = CreateFrame("Button",name.."ColorSwatch",button);
 	color:Hide();
@@ -198,17 +187,24 @@ function lib.Create_DropDownMenuButton(name,parent,opts)
 	color:SetScript("OnEnter",buttonColorSwatchOnEnter);
 	color:SetScript("OnLeave",buttonColorSwatchOnLeave);
 	color:SetNormalTexture([[Interface\ChatFrame\ChatFrameColorSwatch]]);
+	--button.ColorSwatch = color;
 
 	local swatchBg = color:CreateTexture(name.."ColorSwatchSwatchBg","BACKGROUND");
 	swatchBg:SetSize(14,14);
 	swatchBg:SetPoint("CENTER",0,0);
 	swatchBg:SetVertexColor(1,1,1);
+	button.ColorSwatchSwatchBg = swatchBg;
 
-	button.arrow = CreateFrame("Frame",name.."ExpandArrow",button);
-	button.arrow:SetSize(16,16);
-	button.arrow:SetPoint("RIGHT",0,0);
+	local expandArrow = CreateFrame("Button",name.."ExpandArrow",button);
+	expandArrow:SetMotionScriptsWhileDisabled(true);
+	expandArrow:SetSize(16,16);
+	expandArrow:SetPoint("RIGHT",0,0);
+	expandArrow:SetScript("OnClick",buttonExpandArrowOnClick);
+	expandArrow:SetScript("OnEnter",buttonExpandArrowOnEnter);
+	expandArrow:SetScript("OnLeave",buttonExpandArrowOnLeave);
+	button.ExpandArrow = expandArrow;
 
-	local arrow = button.arrow:CreateTexture(nil,"ARTWORK");
+	local arrow = expandArrow:CreateTexture(nil,"ARTWORK");
 	arrow:SetTexture([[Interface\ChatFrame\ChatFrameExpandArrow]]);
 	arrow:SetAllPoints();
 
@@ -217,8 +213,8 @@ function lib.Create_DropDownMenuButton(name,parent,opts)
 	button.invisibleButton:SetPoint("TOPLEFT");
 	button.invisibleButton:SetPoint("BOTTOMLEFT");
 	button.invisibleButton:SetPoint("RIGHT",color,"LEFT",0,0);
-	button.invisibleButton:SetScript("OnEnter",buttonInvisibleButtonOnEnter);
-	button.invisibleButton:SetScript("OnLeave",buttonInvisibleButtonOnLeave);
+	button.invisibleButton:SetScript("OnEnter",lib.UIDropDownMenuButtonInvisibleButton_OnEnter);
+	button.invisibleButton:SetScript("OnLeave",lib.UIDropDownMenuButtonInvisibleButton_OnLeave);
 
 	return button;
 end
@@ -230,11 +226,11 @@ function lib.Create_DropDownMenuList(name,parent,opts)
 	list:SetFrameStrata("FULLSCREEN_DIALOG");
 	list:EnableMouse(true);
 	list:SetScript("OnClick",listOnClick);
-	list:SetScript("OnEnter",listOnEnter);
-	list:SetScript("OnLeave",listOnLeave);
-	list:SetScript("OnUpdate",listOnUpdate);
+	list:SetScript("OnEnter",lib.UIDropDownMenu_StopCounting);
+	list:SetScript("OnLeave",lib.UIDropDownMenu_StartCounting);
+	list:SetScript("OnUpdate",lib.UIDropDownMenu_OnUpdate);
 	list:SetScript("OnShow",listOnShow);
-	list:SetScript("OnHide",listOnHide);
+	list:SetScript("OnHide",lib.UIDropDownMenu_OnHide);
 
 	if opts then
 		if opts.id then
@@ -262,8 +258,13 @@ function lib.Create_DropDownMenuList(name,parent,opts)
 	menuBackdrop:SetBackdropBorderColor(TOOLTIP_DEFAULT_COLOR.r, TOOLTIP_DEFAULT_COLOR.g, TOOLTIP_DEFAULT_COLOR.b);
 	menuBackdrop:SetBackdropColor(TOOLTIP_DEFAULT_BACKGROUND_COLOR.r, TOOLTIP_DEFAULT_BACKGROUND_COLOR.g, TOOLTIP_DEFAULT_BACKGROUND_COLOR.b);
 
-	for i=1, 8 do
+	for i=1, lib.UIDROPDOWNMENU_MAXBUTTONS do
 		lib.Create_DropDownMenuButton(name.."Button"..i,list,{id=i});
+	end
+
+	if not lib.UIDROPDOWNMENU_DEFAULT_TEXT_HEIGHT then
+		local fontName, fontHeight, fontFlags = _G["DropDownList1Button1NormalText"]:GetFont();
+		lib.UIDROPDOWNMENU_DEFAULT_TEXT_HEIGHT = fontHeight;
 	end
 
 	return list;
@@ -280,46 +281,63 @@ function lib.Create_DropDownMenu(name,parent,opts)
 		end
 	end
 
-	local left = menu:CreateTexture(name.."Left","ARTWORK");
-	left:SetSize(25,64);
-	left:SetTexture([[Interface\Glues\CharacterCreate\CharacterCreate-LabelFrame]]);
-	left:SetTexCoord(0,0.1953125,0,1);
-	left:SetPoint("TOPLEFT",0,17);
+	menu.Left = menu:CreateTexture(name.."Left","ARTWORK");
+	menu.Left:SetSize(25,64);
+	menu.Left:SetTexture([[Interface\Glues\CharacterCreate\CharacterCreate-LabelFrame]]);
+	menu.Left:SetTexCoord(0,0.1953125,0,1);
+	menu.Left:SetPoint("TOPLEFT",0,17);
 
-	local middle = menu:CreateTexture(name.."Middle","ARTWORK");
-	middle:SetSize(115,64);
-	middle:SetTexture([[Interface\Glues\CharacterCreate\CharacterCreate-LabelFrame]]);
-	middle:SetTexCoord(0.1953125,0.8046875,0,1);
-	middle:SetPoint("LEFT",left,"RIGHT",0,0);
+	menu.Middle = menu:CreateTexture(name.."Middle","ARTWORK");
+	menu.Middle:SetSize(115,64);
+	menu.Middle:SetTexture([[Interface\Glues\CharacterCreate\CharacterCreate-LabelFrame]]);
+	menu.Middle:SetTexCoord(0.1953125,0.8046875,0,1);
+	menu.Middle:SetPoint("LEFT",menu.Left,"RIGHT",0,0);
 
-	local right = menu:CreateTexture(name.."Right","ARTWORK");
-	right:SetSize(25,64);
-	right:SetTexture([[Interface\Glues\CharacterCreate\CharacterCreate-LabelFrame]]);
-	right:SetTexCoord(0.8046875,1,0,1);
-	right:SetPoint("LEFT",middle,"RIGHT",0,0);
+	menu.Right = menu:CreateTexture(name.."Right","ARTWORK");
+	menu.Right:SetSize(25,64);
+	menu.Right:SetTexture([[Interface\Glues\CharacterCreate\CharacterCreate-LabelFrame]]);
+	menu.Right:SetTexCoord(0.8046875,1,0,1);
+	menu.Right:SetPoint("LEFT",menu.Middle,"RIGHT",0,0);
 
 	menu.Text = menu:CreateFontString(name.."Text","ARTWORK","GameFontHighlightSmall");
 	menu.Text:SetNonSpaceWrap(false);
 	menu.Text:SetJustifyH("RIGHT");
 	menu.Text:SetSize(0,10);
-	menu.Text:SetPoint("RIGHT",right,"RIGHT",-43,2);
+	menu.Text:SetPoint("RIGHT",menu.Right,"RIGHT",-43,2);
 
 	menu.Icon = menu:CreateTexture(name.."Icon","OVERLAY");
 	menu.Icon:Hide();
 	menu.Icon:SetSize(16,16);
 	menu.Icon:SetPoint("LEFT",30,2);
 
-	menu.Button = CreateFrame("Button",name.."Button",menu);
+	local buttonName = name.."Button";
+	menu.Button = CreateFrame("Button",buttonName,menu);
 	menu.Button:SetMotionScriptsWhileDisabled(true);
 	menu.Button:SetSize(24,24);
-	menu.Button:SetPoint("TOPRIGHT",right,"TOPRIGHT",-16,-18);
+	menu.Button:SetPoint("TOPRIGHT",menu.Right,"TOPRIGHT",-16,-18);
 	menu.Button:SetScript("OnEnter",menuButtonOnEnter);
 	menu.Button:SetScript("OnLeave",menuButtonOnLeave);
 	menu.Button:SetScript("OnClick",menuButtonOnClick);
-	menu.Button:SetNormalTexture([[Interface\ChatFrame\UI-ChatIcon-ScrollDown-Up]]);
-	menu.Button:SetPushedTexture([[Interface\ChatFrame\UI-ChatIcon-ScrollDown-Down]]);
-	menu.Button:SetDisabledTexture([[Interface\ChatFrame\UI-ChatIcon-ScrollDown-Disabled]]);
-	menu.Button:SetHighlightTexture([[Interface\Buttons\UI-Common-MouseHilight]],"ADD");
+
+	menu.Button.NormalTexture = menu.Button:CreateTexture(buttonName.."NormalTexture","ARTWORK");
+	menu.Button.NormalTexture:SetTexture([[Interface\ChatFrame\UI-ChatIcon-ScrollDown-Up]]);
+	menu.Button.NormalTexture:SetAllPoints();
+	menu.Button:SetNormalTexture(menu.Button.NormalTexture);
+
+	menu.Button.PushedTexture = menu.Button:CreateTexture(buttonName.."PushedTexture","ARTWORK");
+	menu.Button.PushedTexture:SetTexture([[Interface\ChatFrame\UI-ChatIcon-ScrollDown-Down]]);
+	menu.Button.PushedTexture:SetAllPoints();
+	menu.Button:SetPushedTexture(menu.Button.PushedTexture);
+
+	menu.Button.DisabledTexture = menu.Button:CreateTexture(buttonName.."DisabledTexture","ARTWORK");
+	menu.Button.DisabledTexture:SetTexture([[Interface\ChatFrame\UI-ChatIcon-ScrollDown-Disabled]]);
+	menu.Button.DisabledTexture:SetAllPoints();
+	menu.Button:SetDisabledTexture(menu.Button.DisabledTexture);
+
+	menu.Button.HighlightTexture = menu.Button:CreateTexture(buttonName.."HighlightTexture","ARTWORK");
+	menu.Button.HighlightTexture:SetTexture([[Interface\ChatFrame\UI-Common-MouseHilight]],"ADD");
+	menu.Button.HighlightTexture:SetAllPoints();
+	menu.Button:SetHighlightTexture(menu.Button.HighlightTexture,"ADD");
 
 	return menu;
 end
